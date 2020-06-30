d97cc6f2270af30fb8985d0695ce102b4c3572f0
sahilkandhari <sahil.kandhari10@gmail.com>
2020-06-30 19:17:51 +0530

    Updates

---
+++ precache-manifest.a12d70269bc4b3014257d0a667fa85fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494abcb83d816d98f67a5467a4f0ea35",
+    "url": "/store-ui/index.html"
+  },
+  {
+    "revision": "07b892b31f139e5e9a2f",
+    "url": "/store-ui/static/css/main.0aeccbbf.chunk.css"
+  },
+  {
+    "revision": "5de4c4b973575a20be86",
+    "url": "/store-ui/static/js/2.a66bcd6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/store-ui/static/js/2.a66bcd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b892b31f139e5e9a2f",
+    "url": "/store-ui/static/js/main.394a5e4f.chunk.js"
+  },
+  {
+    "revision": "12375446b1c0e21baa2f",
+    "url": "/store-ui/static/js/runtime-main.2cc9178c.js"
+  },
+  {
+    "revision": "c0a68432906803c1201abfc13be72a5e",
+    "url": "/store-ui/static/media/Carrot.c0a68432.jpg"
+  },
+  {
+    "revision": "6abcdfc07e2b511b2b2a419366349bc6",
+    "url": "/store-ui/static/media/ChilliFlakes.6abcdfc0.jpg"
+  },
+  {
+    "revision": "28ddaa7aeee7090be5e22b97a43ca223",
+    "url": "/store-ui/static/media/logo2.28ddaa7a.jpg"
+  }
+]);
